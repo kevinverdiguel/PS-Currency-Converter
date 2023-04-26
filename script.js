c0baf6f9952a7a80b02f819547c80a319ab01c3c
Kevin Verdiguel <kevinvdflrs@gmail.com>
2023-04-25 22:01:37 -0600

    Primer commit
    Ya puede cargarse a chrome

---
+++ script.js
@@ -0,0 +1,15 @@
+async function convertToUSD(){
+    const collection = document.getElementsByClassName("psw-m-r-3");
+
+    console.log(collection);
+    console.log(1);
+    //Implementar QuerySelector
+    //const number = parseInt(numberElement.innerText);
+
+    //const result = number * 18;
+
+    //resultElement.innerText = result;
+
+}
+
+convertToUSD();
