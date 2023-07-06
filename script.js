b4ca2a7e2d7076f008edd0a332822b887d6d273d
Kevin Verdiguel <kevinvdflrs@gmail.com>
2023-07-06 00:26:44 -0600

    Testing de modificacion html

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-async function convertToUSD(){
-    const collection = document.getElementsByClassName("psw-m-r-3");
+async function convertToUSD() {
+    const original = document.querySelector('span.psw-t-title-m');
+    const elements = document.querySelectorAll('span.psw-t-title-m');
+  
+    if (elements.length > 0) {
+      // Iterate over each element
+      elements.forEach((element) => {
+        // Modify the data inside each element
+        element.textContent = 'US$99.99';
+        console.log(element);
+      });
+  
+      // Replace the original element with the modified element's outerHTML
+      if (original) {
+        elements.forEach((updatedElement) => {
+          elements.outerHTML = updatedElement.outerHTML;
+          console.log(original);
+        });
+      }
+    }
+  }
+  
+  convertToUSD();
 
-    console.log(collection);
-    console.log(1);
-    //Implementar QuerySelector
-    //const number = parseInt(numberElement.innerText);
-
-    //const result = number * 18;
-
-    //resultElement.innerText = result;
-
-}
-
-convertToUSD();
+/*async function convertToUSD() {
+    const original = document.querySelector('span.psw-t-title-m[data-qa="mfeCtaMain#offer0#finalPrice"]');
+    const element = document.querySelector('span.psw-t-title-m');
+  
+    if (element) {
+      // Modify the data inside the element
+      element.textContent = 'US$99.99';
+      console.log(element);
+  
+      if (original) {
+        // Replace the original element with the modified element's outerHTML
+        original.outerHTML = element.outerHTML;
+      }
+    }
+  }
+  
+  convertToUSD();*/
