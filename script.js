3868059447a557a236a2f6c9c099e31579508242
Dell <Dell@DESKTOP-NOUQ59H>
2023-07-07 11:58:54 -0600

    Puede leer precios y volverlos en un float

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
-async function convertToUSD() {
+async function convertPrice() {
     const original = document.querySelectorAll('span.psw-t-title-m');
     const elements = document.querySelectorAll('span.psw-t-title-m');
+    const cambios = document.querySelector('span.psw-t-title-m');
+    let text = [];
+    let floatValue = [];
 
     if(original.length > 0){
       original.forEach((originals) => {
         const tempElement = document.createElement('div');
         tempElement.innerHTML = originals.innerHTML;
+        const currentText = tempElement.textContent;
+        text.push(currentText);
+        //Muestra el texto de los precios en pantalla
+        //console.log(text)
+        
+        
+      });
+      if(text.length > 0){
+        text.forEach((precios) => {
+          const precio = precios;
+          const numericString = precio.replace('US$', '');
+          //floatValue = parseFloat(numericString);
+          floatValue.push(parseFloat(numericString).toFixed(3));
+          console.log(floatValue);
+        });
+    }
 
-        const text = tempElement.textContent;
-        //console.log(text);  
-
+    if(floatValue.length > 0){
+      floatValue.forEach((precios)=>{
+        const precio = precios * 1.16;
+        console.log(precio);
       });
     }
-  
-    if (elements.length > 0) {
+
+    
+    /*if (elements.length > 0) {
       // Iterate over each element
       elements.forEach((element) => {
         const text = element.textContent;
-        const number = parseFloat(text.match(/\d+/)[0]);
-        console.log(number);
+        //const number = parseFloat(text.match(/\d+/)[0]);
+        //console.log(number);
         // Modify the data inside each element
         element.textContent = 'US$99.99';
         console.log(element);
-      });
-  
-      // Replace the original element with the modified element's outerHTML
-      if (original) {
-        elements.forEach((updatedElement) => {
-          elements.outerHTML = updatedElement.outerHTML;
-          console.log(original);
-        });
-      }
-    }
-  }
-  
-  convertToUSD();
-
-/*async function convertToUSD() {
-    const original = document.querySelector('span.psw-t-title-m[data-qa="mfeCtaMain#offer0#finalPrice"]');
-    const element = document.querySelector('span.psw-t-title-m');
-  
-    if (element) {
-      // Modify the data inside the element
-      element.textContent = 'US$99.99';
-      console.log(element);
-  
-      if (original) {
         // Replace the original element with the modified element's outerHTML
-        original.outerHTML = element.outerHTML;
-      }
-    }
-  }
+        elements.forEach((updatedElement) => {
+          element.outerHTML = updatedElement.outerHTML;
+        });
+      });*/
+
+
   
-  convertToUSD();*/
+      
+  }
+}
+convertPrice();
