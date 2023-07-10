6e30381b5f5f6600c67d16b17916648ead83a38d
Dell <Dell@DESKTOP-NOUQ59H>
2023-07-10 13:24:38 -0600

    Ya transforma el precio a MXN y agrega el impuesto

--- script.js
+++ script.js
@@ -4,8 +4,53 @@ async function convertPrice() {
     const cambios = document.querySelector('span.psw-t-title-m');
     let text = [];
     let floatValue = [];
+    var cambioAMXN;
 
-    if(original.length > 0){
+    var requestURL = 'https://api.exchangerate.host/latest?&base=USD'; 
+    var request = new XMLHttpRequest(); 
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    
+    request.onload = function() {
+      var response = request.response;
+      console.log(response);
+      //console.log(response.rates.MXN);
+      cambioAMXN = response.rates.MXN;
+      parseFloat(cambioAMXN);
+      console.log(cambioAMXN);
+      if(original.length > 0){
+        original.forEach((originals) => {
+          const tempElement = document.createElement('div');
+          tempElement.innerHTML = originals.innerHTML;
+          const currentText = tempElement.textContent;
+          text.push(currentText);
+          //Muestra el texto de los precios en pantalla
+          //console.log(text)
+          
+          
+        });
+        if(text.length > 0){
+          text.forEach((precios) => {
+            const precio = precios;
+            const numericString = precio.replace('US$', '');
+            //floatValue = parseFloat(numericString);
+            floatValue.push(parseFloat(numericString).toFixed(3));
+            console.log(floatValue);
+          });
+      }
+  
+      if(floatValue.length > 0){
+        floatValue.forEach((precios)=>{
+          const precio = precios * cambioAMXN;
+          const precioImpuesto = (precios * 1.16) * cambioAMXN;
+          console.log(precio);
+          console.log(precioImpuesto);
+        });
+    }
+
+
+   /* if(original.length > 0){
       original.forEach((originals) => {
         const tempElement = document.createElement('div');
         tempElement.innerHTML = originals.innerHTML;
@@ -28,13 +73,13 @@ async function convertPrice() {
 
     if(floatValue.length > 0){
       floatValue.forEach((precios)=>{
-        const precio = precios * 1.16;
+        const precio = precios * cambioAMXN;
         console.log(precio);
       });
     }
 
     
-    /*if (elements.length > 0) {
+    if (elements.length > 0) {
       // Iterate over each element
       elements.forEach((element) => {
         const text = element.textContent;
@@ -53,5 +98,6 @@ async function convertPrice() {
   
       
   }
+}
 }
 convertPrice();
