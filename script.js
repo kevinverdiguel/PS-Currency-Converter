d8b01a913123800db402c8000c9ea01c499235e0
Dell <Dell@DESKTOP-NOUQ59H>
2023-07-10 13:48:55 -0600

    Boton de activacion front-end agregado

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ async function convertPrice() {
       cambioAMXN = response.rates.MXN;
       parseFloat(cambioAMXN);
       console.log(cambioAMXN);
-      if(original.length > 0){
+
+      /*if(original.length > 0){
         original.forEach((originals) => {
           const tempElement = document.createElement('div');
           tempElement.innerHTML = originals.innerHTML;
@@ -29,7 +30,23 @@ async function convertPrice() {
           //console.log(text)
           
           
-        });
+        });*/
+
+        if (elements.length > 0) {
+          // Iterate over each element
+          elements.forEach((element) => {
+            const text = element.textContent;
+            //const number = parseFloat(text.match(/\d+/)[0]);
+            //console.log(number);
+            // Modify the data inside each element
+            element.textContent = 'US$99.99';
+            console.log(element);
+            // Replace the original element with the modified element's outerHTML
+            elements.forEach((updatedElement) => {
+              element.outerHTML = updatedElement.outerHTML;
+            });
+          });
+
         if(text.length > 0){
           text.forEach((precios) => {
             const precio = precios;
@@ -48,55 +65,6 @@ async function convertPrice() {
           console.log(precioImpuesto);
         });
     }
-
-
-   /* if(original.length > 0){
-      original.forEach((originals) => {
-        const tempElement = document.createElement('div');
-        tempElement.innerHTML = originals.innerHTML;
-        const currentText = tempElement.textContent;
-        text.push(currentText);
-        //Muestra el texto de los precios en pantalla
-        //console.log(text)
-        
-        
-      });
-      if(text.length > 0){
-        text.forEach((precios) => {
-          const precio = precios;
-          const numericString = precio.replace('US$', '');
-          //floatValue = parseFloat(numericString);
-          floatValue.push(parseFloat(numericString).toFixed(3));
-          console.log(floatValue);
-        });
-    }
-
-    if(floatValue.length > 0){
-      floatValue.forEach((precios)=>{
-        const precio = precios * cambioAMXN;
-        console.log(precio);
-      });
-    }
-
-    
-    if (elements.length > 0) {
-      // Iterate over each element
-      elements.forEach((element) => {
-        const text = element.textContent;
-        //const number = parseFloat(text.match(/\d+/)[0]);
-        //console.log(number);
-        // Modify the data inside each element
-        element.textContent = 'US$99.99';
-        console.log(element);
-        // Replace the original element with the modified element's outerHTML
-        elements.forEach((updatedElement) => {
-          element.outerHTML = updatedElement.outerHTML;
-        });
-      });*/
-
-
-  
-      
   }
 }
 }
